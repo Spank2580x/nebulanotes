b8091be6502a074859dd30c78c54a4150de033e0
LucaCampana <tsac028@tecnicosuperiorekennedy.it>
2016-05-12 16:46:49 +0200

    Tu che sei il cantante degli angra.... TI DOVRESTI VERGOGNAREEEEEEEEEEEEE

--- scripts/controllers/homeController.js
+++ scripts/controllers/homeController.js
@@ -1,6 +1,37 @@
 angular.module('routerApp')
-    .controller('homeController', function ($scope){
+    .controller('homeController', function ($scope, NotesFactory, StorageFactory, NotesService){
 
-        $scope.title = "Nebulah Notes";
-       
+        $scope.title = "Nebula Notes";
+
+        var loadingNotes = true;
+        var localStoredNotes = {};
+
+        function init(){
+            alert("Vai");
+            StorageFactory.loadStorage("TODO", function(err, result){
+                if (err) offlineLoading();
+                else {
+                    console.log("Caricamento delle note online");
+                    localStoredNotes = result;
+                }
+            })
+            loadingNotes = false;
+        }
+
+        function offlineLoading(){
+            console.log("Caricamento delle note offline");
+            NotesFactory.loadNotes("TODO", function(err, result){
+                if (err){
+                    //Caricamento delle note fallito
+                    alert("Caricamento delle note fallito");
+                }
+                else {
+                    //Caricamento delle note offline
+                    localStoredNotes = result;
+                }
+            })
+        }
+
+        init();
+
     });
